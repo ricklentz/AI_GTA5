fb2216f8370bde7868a50abc69617b80170bbb1a
RWL <rlentz@gmail.com>
2018-07-21 22:56:58 -0500

    kk

--- data_collection/key_cap.py
+++ data_collection/key_cap.py
@@ -5,6 +5,58 @@ Module for reading keys from a keyboard
 """
 
 import win32api as wapi
+import time
+import win32gui
+
+# Done by Frannecklp
+def grab_screen(winName: str = "Grand Theft Auto V"):
+    desktop = win32gui.GetDesktopWindow()
+
+    # get area by a window name
+    gtawin = win32gui.FindWindow(None, winName)
+    # get the bounding box of the window
+    left, top, x2, y2 = win32gui.GetWindowRect(gtawin)
+    # cut window boarders
+    top += 32
+    left += 3
+    y2 -= 4
+    x2 -= 4
+    width = x2 - left + 1
+    height = y2 - top + 1
+
+    # the device context(DC) for the entire window (title bar, menus, scroll bars, etc.)
+    hwindc = win32gui.GetWindowDC(desktop)
+    # Create a DC object from an integer handle
+    srcdc = win32ui.CreateDCFromHandle(hwindc)
+    # Create a memory device context that is compatible with the source DC
+    memdc = srcdc.CreateCompatibleDC()
+    # Create a bitmap object
+    bmp = win32ui.CreateBitmap()
+    # Create a bitmap compatible with the specified device context
+    bmp.CreateCompatibleBitmap(srcdc, width, height)
+    # Select an object into the device context.
+    memdc.SelectObject(bmp)
+    # Copy a bitmap from the source device context to this device context
+    # parameters: destPos, size, dc, srcPos, rop(the raster operation))
+    memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
+
+    # the bitmap bits
+    signedIntsArray = bmp.GetBitmapBits(True)
+    # form a 1-D array initialized from text data in a string.
+    img = np.fromstring(signedIntsArray, dtype='uint8')
+    img.shape = (height, width, 4)
+
+    # Delete all resources associated with the device context
+    srcdc.DeleteDC()
+    memdc.DeleteDC()
+    # Releases the device context
+    win32gui.ReleaseDC(desktop, hwindc)
+    # Delete the bitmap and freeing all system resources associated with the object.
+    # After the object is deleted, the specified handle is no longer valid.
+    win32gui.DeleteObject(bmp.GetHandle())
+
+    return cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
+
 
 keyList = ["\b"]
 for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789,.'£$/\\":
@@ -18,16 +70,51 @@ def key_check():
             keys.append(key)
     return keys
 
+control_tick_per_second = 15
+cx_last = 0
+cy_last = 0
+keys_list = []
+mouse_last = time.time()
+keys_last_sent = time.time()
+
 while(True):
-	if win32api.GetAsyncKeyState(ord('Q')):
+
+	mouse_change = False
+
+	if wapi.GetAsyncKeyState(ord('Q')):
 		sys.exit()
-	current = win32api.GetCursorPos()
+	current = wapi.GetCursorPos()
 	cx = current[0]
-	cy = current[1]	
-	print(cx + ', ' + cy)
-	for key in keyList:
-        if wapi.GetAsyncKeyState(ord(key)):
-        	print(key)
+	cy = current[1]
+
+	# handle deltas in mouse controls 
+	if (cx_last != cx):
+		cx_last = cx
+		mouse_change = True
+	if (cy_last != cy):
+		cy_last = cy
+		mouse_change = True
+
+	if mouse_change == True:
+		current = time.time()
+		tick = current - mouse_last
+		print(str(current) + ' : ' + str(cx) + ', ' + str(cy) + ' ' + str(tick))
+		mouse_last = current
+
+	# handle deltas in keyboard controls
+	keys_current = key_check()
+	#if len(set(keys_current).intersection(keys_list)) != len(keys_current):
+	current = time.time()
+	tick = current - keys_last_sent
+	keys_list = keys_current
+	key_change = True
+	if (len(keys_current) > 0) & (tick > (1/15)):
+		print(str(current) + ' : ' + ','.join(str(x) for x in keys_current) + ' ' + str(tick))
+		keys_last_sent = current
+
+	# get an image of the desktop
+	file = grab_screen()
+		
 # import sys
 # import time
 # import win32api
